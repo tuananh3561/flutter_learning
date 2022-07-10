af16080f6ae882adb79a7d7a1e94a7203a2ce218
tuananh3561 <72615432+tuananh3561@users.noreply.github.com>
2022-07-11 02:45:59 +0700

    update Containers & Padding

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,15 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // child: Icon(
-        //   Icons.ac_unit,
-        //   color: Colors.red,
-        //   size: 50.0,
-        // ),
-        // child: RaisedButton(
-        //   onPressed: () {},
-        //   child: Text("Click me"),
-        //   color: Colors.lightBlue,
-        // ),
-        // child: FlatButton(
-        //   onPressed: () {
-        //     print("you click me");
-        //   },
-        //   child: Text("Click me"),
-        //   color: Colors.lightBlue,
-        // ),
-        // child: RaisedButton.icon(
-        //   onPressed: () {
-        //     print("you click me");
-        //   },
-        //   icon: Icon(Icons.mail),
-        //   label: Text("click me"),
-        //   color: Colors.lightBlue,
-        // ),
-        child: IconButton(
-          onPressed: () {
-            print("you click me");
-          },
-          icon: Icon(Icons.mail),
-          color: Colors.lightBlue,
-        ),
+      // body: Container(
+      //   margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+      //   padding: EdgeInsets.all(50.0),
+      //   color: Colors.grey[400],
+      //   child: Text("hello"),
+      // ),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text("hello"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
