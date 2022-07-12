bc8f160e570fc1660532b0cb7e847e7596eac53e
tuananh3561 <72615432+tuananh3561@users.noreply.github.com>
2022-07-12 23:17:13 +0700

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,29 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: "this is the qite  text", author: "oscar wilde"),
   ];
 
+  Widget quoteTemplate(Quote quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(quote.text, style: TextStyle(
+              fontSize: 18,
+              color: Colors.grey[600],
+            ),),
+            SizedBox(height: 6.0,),
+            Text(quote.author, style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey[800],
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +55,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text("${quote.text} - ${quote.author}")).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
