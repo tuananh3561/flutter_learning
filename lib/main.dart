e7601676b360601ec5616cbbc60c963f870fd4e7
tuananh3561 <lecongtuananh3561@gmail.com>
2024-03-31 12:33:38 +0700

    programming_basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,166 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  // VARIABLES: You can store different types of data in variables.
+  String name = "John Doe";
+  int age = 30;
+  double pi = 3.14;
+  bool isBeginner = true;
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  /*
+  BASIC MATH OPERATIONS:
+  1 + 1 -> 2 , add
+  4 - 1 -> 3 , subtract
+  2 * 3 -> 6 , munltiply
+  8 / 2 -> 4 , divide
+  9 % 4 -> 1 , remainder
+  5++ -> 6 , increment
+  5-- -> 4 , decrement
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  COMPARISON OPERATORS:
+  5 == 5 -> true , equal to
+  2 != 5 -> true , not equal to
+  3 > 2 -> true , greater than
+  3 < 2 -> false , less than
+  5 >= 5 -> true , greater than or equal to
+  3 <= 2 -> false , less than or equal to
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  LOGICAL OPERATORS:  
+  AND operator, returns true if both sides are true
+  isBeginner && ( age > 18 ) -> return true
 
-  final String title;
+  OR operator, returns true if one side is true
+  isBeginner || ( age < 18 ) -> return true
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  NOT operator, returns the opposite of the value
+  !isBeginner -> return false
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  ------------------------------------------------
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  C O N T R O L F L O W
+
+  if (condition) {
+    // code to run if condition is true
   }
 
+  if (condition) {
+    // code to run if condition is true
+  } else {
+    // code to run if condition is false
+  }
+
+  switch (expression) {
+    case value1:
+      // code to run if expression is value1
+      break;
+    case value2:
+      // code to run if expression is value2
+      break;
+    default:
+      // code to run if expression is not any of the above
+  }
+
+  for loop
+  for (initialization; condition; increment) {
+    // code to run
+  }
+
+  while loop
+  while (condition) {
+    // code to run
+  }
+
+  do while loop
+  do {
+    // code to run
+  } while (condition);
+
+  break -> breaks out of a loop
+  continue -> skips the current iteration of a loop
+
+  ------------------------------------------------
+
+  F U N C T I O N S / M E T H O D S
+
+  */
+
+  // basic function
+  void greet() {
+    print("Hello, John Doe");
+  }
+
+  // function with parameters
+  void greetPerson(String name) {
+    print("Hello, $name");
+  }
+
+  // function with return type
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  /*
+  ------------------------------------------------
+
+  D A T A S T R U C T U R E S
+
+  */
+
+  // LIST: ordered collection of elements, can have duplicates
+  List<int> numbers = [1, 2, 3, 4, 5];
+  // numbers[0] -> 1
+  // numbers[1] -> 2
+  // numbers[2] -> 3
+  // numbers[3] -> 4
+  // numbers[4] -> 5
+
+  void printNumbers() {
+    for (int i = 0; i < numbers.length; i++) {
+      print(numbers[i]);
+    }
+  }
+
+  List<String> names = [
+    "John",
+    "Jane",
+    "Doe",
+    "Jane",
+  ];
+  // names[0] -> John
+  // names[1] -> Jane
+  // names[2] -> Doe
+  // names[3] -> Jane
+
+  // SET: underordered collection of unique elements
+  Set<String> uniqueNames = {"John", "Jane", "Doe"};
+
+  // MAP: collection of key-value pairs
+  Map user = {
+    "name": "John Doe",
+    "age": 30,
+    "isBeginner": true,
+  };
+  // user["name"] -> John Doe
+  // user["age"] -> 30
+  // user["isBeginner"] -> true
+
+  void printNames() {
+    for (int i = 0; i < names.length; i++) {
+      print(names[i]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
