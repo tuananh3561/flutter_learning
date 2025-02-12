88f632095126bc76aaf1cc0fa1d0553ada56f584
tuananh3561 <lecongtuananh3561@gmail.com>
2025-02-12 23:02:09 +0700

    add nakama real time server

--- lib/bloc/game/game_cubit.dart
+++ lib/bloc/game/game_cubit.dart
@@ -1,16 +1,40 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flappy_dash/audio_helper.dart';
-import 'package:meta/meta.dart';
+import 'package:nakama/nakama.dart';
 
 part 'game_state.dart';
 
 class GameCubit extends Cubit<GameState> {
   GameCubit(
     this._audioHelper,
-  ) : super(const GameState());
+  ) : super(const GameState()) {
+    _initializeNakama();
+  }
 
   final AudioHelper _audioHelper;
+
+  void _initializeNakama() async {
+    final client = getNakamaClient(
+      host: '127.0.0.1',
+      ssl: false,
+      serverKey: 'defaultkey',
+      grpcPort: 7349, // optional
+      httpPort: 7350, // optional
+    );
+    final session = await client.authenticateDevice(
+      deviceId: 'test-device-id',
+      username: 'tuananh',
+    );
+    print("Session is: ${session.token}");
+
+    final group = await client.createGroup(
+      session: session,
+      name: "Flutter devs",
+      description: 'This is a cool group for Flutter devs!',
+    );
+    print("Group is created: ${group.id}");
+  }
 
   void startPlaying() {
     _audioHelper.playBackgroundAudio();
